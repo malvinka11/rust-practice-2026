49a4d84376b1c792b8941cc7421906d14237ff79
malvinka11 <viktoria.matvienko@e-u.edu.ua>
2026-03-10 00:51:19 +0100

    practice 6 kangaroo

--- src/hackerrank/task06.rs
+++ src/hackerrank/task06.rs
@@ -1,1 +1,19 @@
-// https://www.hackerrank.com/challenges/kangaroo/problem
+pub fn kangaroo(x1: i32, v1: i32, x2: i32, v2: i32) -> String {
+    if v1 <= v2 {
+        "NO".to_string()
+    } else if (x2 - x1) % (v1 - v2) == 0 {
+        "YES".to_string()
+    } else {
+        "NO".to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_kangaroo_yes() {
+        assert_eq!(kangaroo(0, 3, 4, 2), "YES");
+    }
+}
