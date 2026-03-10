7ce6b84797092c26a711f1f3d605c7558b3c83bf
malvinka11 <viktoria.matvienko@e-u.edu.ua>
2026-03-10 01:26:44 +0100

    practice 9 migratory birds

--- src/hackerrank/task09.rs
+++ src/hackerrank/task09.rs
@@ -1,1 +1,30 @@
-// https://www.hackerrank.com/challenges/migratory-birds/problem
+pub fn migratory_birds(arr: Vec<i32>) -> i32 {
+    let mut freq = vec![0; 6];
+
+    for bird in arr {
+        freq[bird as usize] += 1;
+    }
+
+    let mut best_id = 1;
+    let mut best_count = freq[1];
+
+    for i in 2..=5 {
+        if freq[i] > best_count {
+            best_count = freq[i];
+            best_id = i as i32;
+        }
+    }
+
+    best_id
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_migratory_birds() {
+        let arr = vec![1, 4, 4, 4, 5, 3];
+        assert_eq!(migratory_birds(arr), 4);
+    }
+}
